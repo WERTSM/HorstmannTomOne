a196ce1e06c1e0be1780f3d9a2bc77a21e555326
WERTSM <wertsm.work@gmail.com>
2017-10-17 16:04:01 +0300

    log

---
+++ src/v1ch00myExperiences/logging/LogPract.java
@@ -0,0 +1,14 @@
+package v1ch00myExperiences.logging;
+
+import java.util.logging.Logger;
+
+/**
+ * Created by WERT on 17.10.2017.
+ */
+public class LogPract {
+    public static void main(String[] args) {
+        System.out.println("Привет");
+        Logger.getGlobal().info("l,l,l");
+    }
+
+}
