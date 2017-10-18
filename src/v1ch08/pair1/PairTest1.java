f8fa82587ead01f23c67bc39853a7297c726ffaa
WERTSM <wertsm.work@gmail.com>
2017-10-18 15:38:51 +0300

    generic

--- src/v1ch08/pair1/PairTest1.java
+++ src/v1ch08/pair1/PairTest1.java
@@ -10,6 +10,7 @@ public class PairTest1
    {
       String[] words = { "Mary", "had", "a", "little", "lamb" };
       Pair<String> mm = ArrayAlg.minmax(words);
+
       System.out.println("min = " + mm.getFirst());
       System.out.println("max = " + mm.getSecond());
    }
