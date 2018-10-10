85d7f8dd77a2538f2060c2d098d6d20a6e276f80
WERTSM <wertsm.work@gmail.com>
2018-10-10 23:32:33 +0300

    StringTest

---
+++ src/v1ch00myExperiences/raznoe/a1/String/StringConsole.java
@@ -0,0 +1,4 @@
+package v1ch00myExperiences.raznoe.a1.String;
+
+public class StringConsole {
+}
---
+++ src/v1ch00myExperiences/raznoe/a1/String/StringTest.java
@@ -0,0 +1,9 @@
+package v1ch00myExperiences.raznoe.a1.String;
+
+public class StringTest {
+    public static void main(String[] args) {
+        String str = "Spartak Champion";
+        String strRus = "Спартак Чемпион";
+        System.out.println(str + "\n" + strRus);
+    }
+}
