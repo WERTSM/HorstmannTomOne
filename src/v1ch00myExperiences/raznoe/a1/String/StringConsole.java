6eaae6e9ae37d4812ad9d7d374f3bc15b62a1bdd
WERTSM <wertsm.work@gmail.com>
2018-10-16 23:40:28 +0300

    String Console

--- src/v1ch00myExperiences/raznoe/a1/String/StringConsole.java
+++ src/v1ch00myExperiences/raznoe/a1/String/StringConsole.java
@@ -1,5 +1,8 @@
 package v1ch00myExperiences.raznoe.a1.String;
 
 public class StringConsole {
+  public static void main (String ... args){
   String str = new Console("Stroka");
+  System.out.println(str);
+  }
 }
