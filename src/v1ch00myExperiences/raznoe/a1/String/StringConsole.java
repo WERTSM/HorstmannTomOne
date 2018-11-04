d4cba2573e72fea42a1a1592e91fc42f4755b8c1
WERTSM <wertsm.work@gmail.com>
2018-11-04 23:38:29 +0300

    depression

--- src/v1ch00myExperiences/raznoe/a1/String/StringConsole.java
+++ src/v1ch00myExperiences/raznoe/a1/String/StringConsole.java
@@ -3,7 +3,7 @@ package v1ch00myExperiences.raznoe.a1.String;
 import java.io.Console;
 
 public class StringConsole {
-  public static void main (String [] args){
+  //public static void main (String [] args){
   //Console cns = new Console ("A tutti avanti Massimo Carrera!!!!!!!!!!!!!!!!!! ");
   //String str = new Console ();
   //System.out.println();
